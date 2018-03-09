50c6159be0c6affd4d627b85db0f5b4b271b446a
jhnmthw <jhn.mthwjp@gmail.com>
2018-03-09 09:31:34 +0530

    Enhanced for reading and calculating number of VUsers in enabled thread groups from jmx files

--- src/main/java/com/tavant/nfr/meagan/jmeter/ExcelSheetManager.java
+++ src/main/java/com/tavant/nfr/meagan/jmeter/ExcelSheetManager.java
@@ -29,13 +29,18 @@ import org.jxls.util.JxlsHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.aspose.cells.CellArea;
+import com.aspose.cells.DataSorter;
+import com.aspose.cells.SortOrder;
+
+
 public class ExcelSheetManager {
 
 	private static String templatePath = "target/jmeter/Jmeter_Reporting_Template.xlsx";
 	private static String reportPath = "target/jmeter/results/Report-" + new SimpleDateFormat("yyyyMMdd").format(Calendar.getInstance().getTime()) + ".xlsx";
 	static Logger logger = LoggerFactory.getLogger(ExcelSheetManager.class);
 
-	public static void main(String args[]) throws ParseException, IOException {
+	public static void main(String args[]) throws Exception {
 		
 		logger.info("Processing JMeter Report Template....");
 		
@@ -63,25 +68,111 @@ public class ExcelSheetManager {
 		errorSummary.getSummary(new File(System.getProperty("maven.multiModuleProjectDirectory")
 				+ "/target/jmeter/results/SynthesisReport-" + LocalDate.now().format(errorSummary.formatter) + "-errors.csv"));
 		
-		List<JmeterResultsSummary> summarys = new ArrayList<JmeterResultsSummary>();
-		summarys.add(successSummary);
-		summarys.add(allSummary);
-		summarys.add(errorSummary);
+		List<JmeterResultsSummary> summarys1 = new ArrayList<JmeterResultsSummary>();
+		List<JmeterResultsSummary> summarys2 = new ArrayList<JmeterResultsSummary>();
+		List<JmeterResultsSummary> summarys3 = new ArrayList<JmeterResultsSummary>();
+		
+		summarys1.add(successSummary);
+			
+		summarys2.add(allSummary);
 		
 
+		summarys3.add(errorSummary);
 		
 		logger.info("Creating JMeter Report from Template....");
+		
+		createReport(summarys1,summarys2,summarys3);
+		
+//		Transactions t[] = new Transactions[successSummary.samplerLabels.size()];
+//		
+//		for(int i=0;i<successSummary.samplerLabels.size();i++)
+//		t[i] = new Transactions(successSummary.samplerLabels.get(i), Double.parseDouble(successSummary.averageResponseTimeList.get(i)));
+//		
+//		Arrays.sort(t);
+//		
+//		tList.add(t);
+//		
+		
+		
+		
 
-		createReport(summarys);
+		
+		com.aspose.cells.Workbook reportWorkbook = new com.aspose.cells.Workbook(reportPath);
+		
+		//Obtain the DataSorter object in the workbook
+		DataSorter sorter = reportWorkbook.getDataSorter();
+		 
+		//Set the first order
+		sorter.setOrder1(SortOrder.ASCENDING);
+		 
+		//Define the first key.
+		sorter.setKey1(0);
+		 
+		//Set the second order
+		sorter.setOrder2(SortOrder.ASCENDING);
+		 
+		//Define the second key
+		sorter.setKey2(1);
+		 
+		//Create a cells area (range).
+		CellArea ca1 = new CellArea();
+		CellArea ca2 = new CellArea();
+		CellArea ca3 = new CellArea();
+		 
+		//Specify the start row index.
+		ca1.StartRow = 12;
+		//Specify the start column index.
+		ca1.StartColumn = 1;
+		//Specify the last row index.
+		ca1.EndRow = successSummary.samplerLabels.size()+1;
+		//Specify the last column index.
+		ca1.EndColumn = 11;
+		 
+		//Sort data in the specified data range (A2:C10)
+		sorter.sort(reportWorkbook.getWorksheets().get(0).getCells(), ca1);
+		
+		//Specify the start row index.
+		ca2.StartRow = 3;
+		//Specify the start column index.
+		ca2.StartColumn = 1;
+		//Specify the last row index.
+		ca2.EndRow = allSummary.samplerLabels.size();
+		//Specify the last column index.
+		ca2.EndColumn = 11;
+
+		
+		sorter.sort(reportWorkbook.getWorksheets().get(1).getCells(), ca2);
+		
+		//Specify the start row index.
+		ca3.StartRow = 6;
+		//Specify the start column index.
+		ca3.StartColumn = 1;
+		//Specify the last row index.
+		ca3.EndRow = errorSummary.samplerLabels.size()+1;
+		//Specify the last column index.
+		ca3.EndColumn = 11;
+		
+		sorter.sort(reportWorkbook.getWorksheets().get(3).getCells(), ca3);
+
+        //Save the excel file.
+		try {
+			reportWorkbook.save(reportPath);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 	
-	private static void createReport(List<JmeterResultsSummary> summarys) throws FileNotFoundException, IOException{
+	private static void createReport(List<JmeterResultsSummary> summarys1,List<JmeterResultsSummary> summarys2,List<JmeterResultsSummary> summarys3) throws FileNotFoundException, IOException{
 		try (FileInputStream is = new FileInputStream(new File(templatePath))) {
 			try (FileOutputStream os = new FileOutputStream(reportPath)) {
 				Context context = new Context();
 				
-				context.putVar("summarys", summarys);
+				context.putVar("summarys1", summarys1);
+				context.putVar("summarys2", summarys2);
+				context.putVar("summarys3", summarys3);
+		//		context.putVar("tList", tList);
 				
 				JxlsHelper.getInstance().processTemplate(is, os, context);
 
